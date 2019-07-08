503f1baf7bce8014e22028ff4bd20c816cf80823
Carolinelovebagel <51379721+chengy14@users.noreply.github.com>
2019-07-08 14:14:19 -0400

    Update remove_duplicate.py

--- remove_duplicate.py
+++ remove_duplicate.py
@@ -1,5 +1,4 @@
 
-#from more_itertools import unique_everseen
 
 with open ("/mnt/home/chengy14/evidence_test_disease_to_drug.csv",'r') as in_file, open('evidence_test_disease_to_drug_removed.csv','w') as out_file:
     seen = set() # set for fast O(1) amortized lookup
