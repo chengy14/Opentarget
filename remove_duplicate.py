c4372225c6d3ec88d7ec07a2ea3ae541f5e9ccce
Carolinelovebagel <51379721+chengy14@users.noreply.github.com>
2019-07-08 12:01:50 -0400

    Update remove_duplicate.py

--- remove_duplicate.py
+++ remove_duplicate.py
@@ -1,3 +1,4 @@
 from more_itertools import unique_everseen
+# drug file is needed to remove duplicates too
 with open ('disease_evidence.csv','r') as f, open('disease_info_removed.csv','w') as out:
      out.writelines(unique_everseen(f))
