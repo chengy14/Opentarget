600ab2861a53d75a308863bc49c3b49a3f351898
Carolinelovebagel <51379721+chengy14@users.noreply.github.com>
2019-07-08 14:17:45 -0400

    Update test_disease_to_drug.py

--- test_disease_to_drug.py
+++ test_disease_to_drug.py
@@ -1,6 +1,6 @@
 import json
 import gzip
-#from pprint import pprint
+# READ data from gz File
 with gzip.open("/mnt/home/chengy14/evidence_data.json.gz", "r") as f, open("evidence_test_disease_to_drug.csv","w") as d:
     d_columns = 'disease_label\tdisease_id\tdisease_therapeutic_area_label\tdrug_name\t\n'
     d.write(d_columns)
@@ -10,7 +10,7 @@ with gzip.open("/mnt/home/chengy14/evidence_data.json.gz", "r") as f, open("evid
     while line:
         # convert to json object
         line = json.loads(line.strip())
-        # d data
+        # check whether dictionary contains 'drug' and 'disease'
         
         for key,value in line.items():
             if key == 'drug':
@@ -19,6 +19,7 @@ with gzip.open("/mnt/home/chengy14/evidence_data.json.gz", "r") as f, open("evid
                disease_label = line['disease']['efo_info']['label']
                disease_id_ = line['disease']['id']
                disease_therapeutic_area_label=line['disease']['efo_info']['therapeutic_area']['labels'][0]
+        # write data into new file
         d.write('\t'.join([str(disease_label),str(disease_id_),str(disease_therapeutic_area_label),str(drug_name),])+ "\n")
         line=f.readline()
     
